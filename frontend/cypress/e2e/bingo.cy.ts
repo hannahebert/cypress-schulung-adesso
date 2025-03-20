0eeda55c0e2133e3c186975f38ec62335c02b972
hannahebert <hannah.ebert90@gmail.com>
2025-03-20 11:37:45 +0100

    update

--- frontend/cypress/e2e/bingo.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-    it('passes', () => {
-        cy.intercept('GET', '**/bingo-items/vacation', {fixture: 'vacation_bingo.json'}).as('vacationItems');
-        cy.login('cypress', 'bingo');
-
-        cy.wait('@vacationItems');
-    })
-})
